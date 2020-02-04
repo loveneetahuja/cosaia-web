5d3e5da2ea14395af07b7af94a8b0cea92102d3d
Carolina <carolinasantos@cosaia.com>
2020-02-04 15:52:39 +0530

    seo optimized version

--- public/web.js
+++ public/web.js
@@ -106,5 +106,26 @@ $('#links').on('show.bs.collapse', function () {
     $('#links').collapse("hide");
   })
 
+// Navbar animation
+
+
+if ( $(window).width() > 769) {   
+var mywindow = $(window);
+var mypos = mywindow.scrollTop();
+var up = false;
+var newscroll;
+mywindow.scroll(function () {
+    newscroll = mywindow.scrollTop();
+    if (newscroll > mypos && !up) {
+        $('.slidd').stop().fadeOut(730);
+        up = !up;
+        // console.log(up);
+    } else if(newscroll < mypos && up) {
+        $('.slidd').stop().fadeIn(300);
+        up = !up;
+    }
+    mypos = newscroll;
+});
+}
 
 
