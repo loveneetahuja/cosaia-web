f7eaf9c63c152aa49f4d6c411d271b5d34351f06
Loveneetahuja <loveneet.ahuja@yahoo.com>
2020-02-15 15:48:10 +0530

    meta tags

--- app.js
+++ app.js
@@ -19,7 +19,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //Restful Routes
 app.get("/", function(req, res){
-    res.render("home");
+
+    var data1 = {
+        "title": "Web Development & Design Company India | Web & Mobile Application", 
+        "description": "Cosaia Private Limited is a Design, Web Development and Digital Marketing Company based in India with several years of experience and clients around the globe. Our large team of highly skilled developers, designers and digital marketing experts will take your business to the next level.", // meta description
+    };
+    res.render("home", {'data': data1});
 })
 
 app.get("/projects", function(req, res){
@@ -35,15 +40,27 @@ app.get("/contact_us", function(req, res){
 })
 
 app.get("/development", function(req, res){
-    res.render("development")
+    var data1 = {
+        "title": "Website Development Company India | Custom Web Development", 
+        "description": "Cosaia Private Limited is a web development company based in India. We provide services such as website development, app development and custom web apps for businesses to automate growth.", // meta description
+    };
+    res.render("development" , {'data': data1});
 })
 
 app.get("/design", function(req, res){
-    res.render("design");
+    var data1 = {
+        "title": "Website Designing Company India | Corporate Logo Design", 
+        "description": "Cosaia Private Limited is a web design company based in India. We provide services such as logo design, branding, corporate identity, stationery design, packaging and UI/UX wireframing and prototyping.", // meta description
+    };
+    res.render("design", {'data': data1});
 })
 
 app.get("/digitalmarketing", function(req,res){
-    res.render("digital_marketing");
+    var data1 = {
+        "title": "Digital Marketing Company India | SEO services ", 
+        "description": "Cosaia Private Limited is a digital marketing agency based in India with clients all over the globe. We provide services such as Social Media Marketing, Email Marketing and Search Engine Optimization (SEO).", // meta description
+    };
+    res.render("digital_marketing" , {'data': data1});
 
 })
 
